f1d65b8db1953f0344e223075df89589ecd0d3eb
Insan-Aleksandr Latypov <48738609+AFigaro@users.noreply.github.com>
2022-10-16 16:56:20 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Issue 1 and 2
+
+The tests and code are in main.py file. These functions were tested 
+together because they are inverse of each other
+
+To run the tests on encode funtion in morse example:
+```python main.py -o IGNORE_EXCEPTION_DETAIL -v```
+
+# Issue 3
+
+The function and tests are given in unittest_ohe.py file
+To run the tests on encode funtion in morse example:
+```python -m unittest -v unittest_ohe.py```
+
+# Issue 4
+
+The function and tests are given in pytest_ohe.py file
+To run the tests on encode funtion in morse example:
+```python -m pytest -v pytest_ohe.py```
---
+++ main.py
@@ -0,0 +1,79 @@
+"""Morse Code Translator"""
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие c таблицей азбуки Морзе
+
+    message: str
+
+    >>> encode('')
+    ''
+    >>> encode('ABBA') # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    ...
+    '.- -... -...   .-'
+    >>> encode('SOS')
+    '... --- ...'
+    >>> encode('#')
+    Traceback (most recent call last):
+        ...
+    KeyError: '#'
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+
+    morse_message: str
+
+    >>> decode('')
+    ''
+    >>> decode('.- -... -.- .-..') # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    ...
+    'ABKL'
+    >>> decode('... --- ...')
+    'SOS'
+    >>> decode('--------')  # doctest: +ELLIPSIS,
+    Traceback (most recent call last):
+        ...
+    KeyError: '--------'
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
